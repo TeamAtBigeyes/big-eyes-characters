924b032bc3936a6bf9d2e333270b1aef2ea7bed1
TeamAtBigeyes <team@bigeyes.space>
2022-07-25 16:39:30 +0100

    :construction: Actual redirect at root.

---
+++ pages/_middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(req, ev) {
+
+  if (req.nextUrl.pathname.startsWith("/api/get-url/")) {
+    console.log("returning early");
+    return;
+  }
+
+  const slug = req.nextUrl.pathname.split("/").pop();
+
+  // console.log(slug);
+
+  const data = await (
+    await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
+  ).json();
+
+  if (data.url) {
+    return NextResponse.redirect(data.url);
+  }
+}
